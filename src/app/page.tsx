017458020f9baaec091556caabcc6f2252a5dabc
s1f10240057 <s1f102400572@iniad.org>
2025-04-19 06:06:26 +0900

    'ターン切り替えができるようになったよ'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,15 +19,12 @@ const Home = () => {
   ]);
 
   const clickHandler = (x: number, y: number) => {
-    console.log(x, y);
     const newBoard = structuredClone(board);
-    newBoard[y][x] = turnColor;
-    setBoard(newBoard);
-    if (turnColor === 1) {
-      setTurnColor(2);
-    } else {
-      setTurnColor(1);
+    if (board[y + 1] !== undefined && board[y + 1][x] === 2 / turnColor) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(2 / turnColor);
     }
+    setBoard(newBoard);
   };
   return (
     <div className={styles.container}>
