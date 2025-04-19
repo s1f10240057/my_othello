db84bce725aaf91e9c9c8c45281f248775d609d9
s1f10240057 <s1f102400572@iniad.org>
2025-04-19 18:23:09 +0900

    '石がひっくり返るようになった'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,12 +22,43 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 1, 2, 2, 2, 0],
     [0, 0, 0, 2, 1, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
+  const Inversionprocessing = (
+    x: number,
+    y: number,
+    count: number,
+    direction: number[],
+    newBoard: number[][],
+  ) => {
+    for (let n: number = 1; n <= count; n++) {
+      newBoard[y + direction[1] * n][x + direction[0] * n] = turnColor;
+      console.log(`n:${n}:${newBoard[y + direction[1] * n][x + direction[0] * n]}`);
+    }
+    setBoard(newBoard);
+  };
+
+  const Inversioncount = (x: number, y: number, direction: number[]) => {
+    let n = 0;
+    let count = 0;
+    while (true) {
+      n += 1;
+      if (board[y + direction[1] * n] === undefined || board[x + direction[0] * n] === undefined) {
+        count = 0;
+        break;
+      } else if (board[y + direction[1] * n][x + direction[0] * n] === turnColor) {
+        break;
+      } else {
+        count += 1;
+      }
+    }
+    return count;
+  };
 
   const clickHandler = (x: number, y: number) => {
     const newBoard = structuredClone(board);
@@ -36,6 +67,12 @@ const Home = () => {
       for (let i: number = 0; i < 7; i++) {
         if (board[y + direction_lst[i][1]][x + direction_lst[i][0]] === 2 / turnColor) {
           newBoard[y][x] = turnColor;
+          for (let i: number = 0; i < 7; i++) {
+            const count = Inversioncount(x, y, direction_lst[i]);
+            if (count !== 0) {
+              Inversionprocessing(x, y, count, direction_lst[i], newBoard);
+            }
+          }
           setTurnColor(2 / turnColor);
           break;
         }
